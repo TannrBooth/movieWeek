53b95ddcac25351e6d90825f84ad669105c038d5
TannrBooth <133698469+TannrBooth@users.noreply.github.com>
2023-07-24 16:54:37 -0600

    first commit

---
+++ src/main/java/com/javaunit3/springmvc/Movie.java
@@ -0,0 +1,10 @@
+package com.javaunit3.springmvc;
+
+import java.util.SplittableRandom;
+
+public interface Movie {
+    public String getTitle();
+    public String getMaturityRating();
+    public String getGenre();
+
+}
